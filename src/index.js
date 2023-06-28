c02c7c9542b2971badfae1f6716d7b11fbeefa5e
Lauraatii <tihanova.laura@gmail.com>
2023-06-28 15:34:35 +0200

    new changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './styles.css';
+import '../src/styles.css';
 
 const numbersDiv = document.getElementById('numbers');
 const combinationsDiv = document.getElementById('combinations');
@@ -7,23 +7,25 @@ let numbers = [];
 document.getElementById('newArray').addEventListener('click', function() {
   numbers = Array.from({length: 9}, () => Math.floor(Math.random() * 21));
   numbersDiv.textContent = numbers.join(', ');
-  combinationsDiv.textContent = '';
+  combinationsDiv.innerHTML = '';
 });
 
 document.getElementById('findCombinations').addEventListener('click', function() {
-  const seenNumbers = {};
   const combinations = [];
-
+  
   for (let i = 0; i < numbers.length; i++) {
-    const number = numbers[i];
-    const difference = 21 - number;
-    
-    if (seenNumbers[difference]) {
-      combinations.push([numbers.indexOf(number), numbers.indexOf(difference)]);
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === 21) {
+        combinations.push([i+1, j+1]); // Adding 1 to each index for 1-based indexing
+      }
     }
-
-    seenNumbers[number] = true;
   }
 
-  combinationsDiv.textContent = combinations.map(c => c.join(' & ')).join('\n');
+  let combinationsList = '<ul class="combinations-list">';
+  combinations.forEach(c => {
+    combinationsList += `<li>${c.join(' & ')}</li>`;
+  });
+  combinationsList += '</ul>';
+
+  combinationsDiv.innerHTML = combinations.length > 0 ? `<p class="combinations-title">Combinations:</p>${combinationsList}` : '';
 });
